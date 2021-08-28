b94fcf468bbaa57ec41ec13fa15fc28aefc0de22
Leon <salient@Full-Stack-SEO.local>
2021-08-28 11:51:23 -0400

    added forloop basic

--- script.py
+++ script.py
@@ -1,9 +1,12 @@
-num1 = num2 = 9000
-print(num1)
-print(num2)
-name = "leon"
-if name == "leon":
-    for character in name:
+# num1 = num2 = 9000
+# print(num1)
+# print(num2)
+# name = "leon"
+# if name == "leon":
+#     for character in name:
+#         print(character)
+# else:
+#     print("name is not leon")
+for character in "Leon":
+    if character == "e":
         print(character)
-else:
-    print("name is not leon")
